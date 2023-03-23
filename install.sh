be09d4c21bdc8e078331a7608bedf380cc7b59b0
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-03-24 00:08:23 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,21 +2,10 @@ echo "Nginx v1.23.3一键搭建脚本"
 echo "脚本作者：舒夏"
 echo "GitHub开源地址：https://github.com/sxbai/nginx-on-replit"
 nix-env -iA nixpkgs.wget
-mkdir build
-cd build
-wget -O .replit https://github.com/sxbai/nginx-on-replit/raw/master/.replit
-wget -O replit.nix https://github.com/sxbai/nginx-on-replit/raw/master/replit.nix
-cd ..
-cp -r build/.replit . && cp -r build/replit.nix .
-wget -O main.sh https://github.com/sxbai/nginx-on-replit/raw/master/main.sh
-wget -O nginx-1.23.3.tar.gz https://nginx.org/download/nginx-1.23.3.tar.gz
-tar -zxvf nginx-1.23.3.tar.gz
-cd nginx-1.23.3
-./configure --prefix=/home/runner/${REPL_SLUG} --with-stream --with-mail --user=replit --group=replit --with-http_ssl_module --with-http_auth_request_module --with-pcre
-make && make install
-cd ..
-rm -rf build/ && rm -rf nginx-1.23.3.tar.gz
-rm -rf nginx-1.23.3/
+wget -O nginx.zip https://github.com/sxbai/nginx-on-replit/raw/master/nginx.zip
+nix-env -iA nixpkgs.unzip
+unzip nginx.zip
+rm -rf nginx.zip
 bash main.sh
 echo "Nginx v1.23.3一键搭建脚本"
 echo "脚本作者：舒夏"
