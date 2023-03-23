7d73640f422b084d7c29c4bd05e21f27c12b7535
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-03-24 00:33:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,13 +7,15 @@ cd build
 wget -O .replit https://github.com/sxbai/nginx-on-replit/raw/master/.replit
 cd ..
 cp -r build/.replit .
+rm -rf build/
 wget -O nginx.zip https://github.com/sxbai/nginx-on-replit/raw/master/nginx.zip
 nix-env -iA nixpkgs.unzip
 unzip nginx.zip
 rm -rf nginx.zip
 cp -r a/.cache .cache
+rm -rf a/
 echo "Nginx v1.23.3一键搭建脚本"
 echo "脚本作者：舒夏"
 echo "GitHub开源地址：https://github.com/sxbai/nginx-on-replit"
 echo "点击Run按钮，启动Nginx项目! 使用愉快!!!"
-rm -rf a/ && rm -rf README.md
+rm -rf README.md
