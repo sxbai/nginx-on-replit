a463d040864185071e5c0094e9b395a7a07a2e10
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-03-30 15:34:26 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,8 @@ wget -O main.sh https://github.com/sxbai/nginx-on-replit/raw/master/main.sh
 mkdir build
 cd build
 wget -O .replit https://github.com/sxbai/nginx-on-replit/raw/master/.replit
-wget -O replit.nix https://github.com/sxbai/nginx-on-replit/raw/master/replit.nix
 cd ..
-cp -r build/.replit . && cp -r build/replit.nix .
-rm -rf build/
+cp -r build/.replit . && rm -rf build/
 wget -O nginx.zip https://github.com/sxbai/nginx-on-replit/raw/master/nginx.zip
 nix-env -iA nixpkgs.unzip
 unzip nginx.zip
